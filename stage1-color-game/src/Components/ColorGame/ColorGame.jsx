98c1512b5a61737b88a81ce67e2ab0aa2cb247ae
IsaacToluwani <toluwani.isaac@gmail.com>
2025-02-07 00:37:36 +0100

    update App Structure

--- stage1-color-game/src/Components/ColorGame/ColorGame.jsx
+++ stage1-color-game/src/Components/ColorGame/ColorGame.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import './ColorGame.css'
+import React from 'react';
 
 function ColorGame() {
+  // State Management
+
   return (
-    <div>ColorGame</div>
-  )
+    <main>
+      <h1>Issaac's Color Guessing Game</h1>
+      <div className="game">
+      <div
+          className='color-box'
+          // style={{ backgroundColor: targetColor }}
+          data-testid='colorBox'
+        ></div>
+        <p className='game-instructions' data-testid='gameInstructions'>
+          Guess the correct color!
+        </p>
+        <p className='game-status' data-testid='gameStatus'>
+          game status
+        </p>
+        <p className='score' data-testid='score'>
+          score:
+        </p>
+        <button
+          className='new-game-button'
+          onClick
+          data-testid='newGameButton'
+        >
+          New Game
+        </button>
+      </div>
+    </main>
+  );
 }
 
-export default ColorGame
+export default ColorGame;
