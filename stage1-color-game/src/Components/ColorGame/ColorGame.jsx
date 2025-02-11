cb90aae663c1d4ca10e471e5ad15c06b42cf998c
IsaacToluwani <toluwani.isaac@gmail.com>
2025-02-11 19:48:42 +0100

    Add create react app

--- stage1-color-game/src/Components/ColorGame/ColorGame.jsx
+++ stage1-color-game/src/Components/ColorGame/ColorGame.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const colors = ['blue', 'red', 'pink', 'purple', 'black', 'green'];
 
 function ColorGame() {
-  // State Management
+  /* State Management */
+  const [chosenColor, setChosenColor] = useState('red');
+  console.log('chosen:', chosenColor);
+  const [shuffledColor, setShuffledColor] = useState('')
+  
+
+  /* Start Game */
+  const startGame = () => {
+    const chooseRandom = colors[Math.floor(Math.random() * colors.length)];
+    setChosenColor(chooseRandom)
+
+    const shuffleColor = [...colors].sort(() => Math.random() - 0.5);
+    
+   
+ }
 
   return (
     <main>
-      <h1>Issaac's Color Guessing Game</h1>
-      <div className="game">
-      <div
+      <h1>Isaac's Color Guessing Game</h1>
+      <div className='game'>
+        <div
           className='color-box'
-          // style={{ backgroundColor: targetColor }}
+          style={{ backgroundColor: chosenColor }}
           data-testid='colorBox'
         ></div>
         <p className='game-instructions' data-testid='gameInstructions'>
           Guess the correct color!
         </p>
+        <div className='choose-color' data-testid='colorOption'></div>
         <p className='game-status' data-testid='gameStatus'>
           game status
         </p>
         <p className='score' data-testid='score'>
           score:
         </p>
-        <button
-          className='new-game-button'
-          onClick
-          data-testid='newGameButton'
-        >
+        <button className='new-game-button' onClick data-testid='newGameButton'>
           New Game
         </button>
       </div>
